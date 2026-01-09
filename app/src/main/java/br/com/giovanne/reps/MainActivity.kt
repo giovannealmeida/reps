4da58b8542e717f4cf28b35b4160900a8edef955
Giovanne Almeida <giovanne.almeida91@gmail.com>
2026-01-08 23:55:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/giovanne/reps/MainActivity.kt
@@ -0,0 +1,97 @@
+package br.com.giovanne.reps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IntegerRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import br.com.giovanne.reps.ui.theme.REPSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            REPSTheme {
+                REPSApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun REPSApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            it.icon,
+                            contentDescription = stringResource(it.labelRes)
+                        )
+                    },
+                    label = { Text(stringResource(it.labelRes)) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+enum class AppDestinations(
+    val labelRes: Int,
+    val icon: ImageVector,
+) {
+    HOME(R.string.destination_home, Icons.Default.Home),
+    FAVORITES(R.string.destination_trains, Icons.Default.Favorite),
+    PROFILE(R.string.destination_history, Icons.Default.AccountBox),
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    REPSTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/br/com/giovanne/reps/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package br.com.giovanne.reps.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Green80 = Color(0xFF7FFF00)
+val Green20 = Color(0xFF1D2F12)
+val Gray80 = Color(0xFFAEAEAE)
+val Gray30 = Color(0xFF3D3D3D)
+val Gray20 = Color(0xFF303030)
+val Red50 = Color(0xFFFF5555)
+val White80 = Color(0xFFE6E6E6)
+val White = Color(0xFFFFFF)
+val Black = Color(0x000000)
